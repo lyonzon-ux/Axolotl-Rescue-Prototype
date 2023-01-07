24d45db815fbd9072cea3c432f2ba8d38c800de1
Lou Yonzon <Marytalens@gmail.com>
2023-01-07 02:25:04 -0800

    added score and increasing amount of enemies

--- src/main.js
+++ src/main.js
@@ -24,15 +24,19 @@ function preload()
     this.load.image('ocean', 'assets/Ocean.png');
     this.load.image('axolotl', 'assets/axo.png');
     this.load.image('bubble', 'assets/bubbleplats.png');
+    this.load.image('bomb', 'assets/spikes.png');
     this.load.spritesheet('friend', 
     'assets/octo.png',
     { frameWidth: 32, frameHeight: 48 }
+    
 );
 }
 
 var platforms;
 var player;
-
+var score=0;
+var scoreText;
+var bombs
 
 function create()
 {
@@ -76,23 +80,29 @@ function create()
     
     cursors = this.input.keyboard.createCursorKeys();
 
-
-    axos= this.physics.add.group({
-        key:'axolotl',
-        repeat: 15,
-        setXY: {x:12, y: 0, stepX: 70}
+    axo = this.physics.add.group({
+        key: 'axolotl',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
     });
 
-    axos.children.iterate(function (child) {
-        
+    axo.children.iterate(function (child) {
+
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
 
     });
+    
+    bombs=this.physics.add.group();
+
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
     this.physics.add.collider(player, platforms);
-    this.physics.add.collider(axolotl, platforms);
+    this.physics.add.collider(axo, platforms);
     
-    this.physics.add.overlap(player, axolotl, collectAxo, null, this);
+
+    this.physics.add.overlap(player,axo,collectAxo,null,this);
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
+
 
 
 }
@@ -129,5 +139,37 @@ if (cursors.up.isDown && player.body.touching.down)
 
 function collectAxo(player, axolotl)
 {
-    star.disableBody(true,true);
+    axolotl.disableBody(true,true);
+    score+=15;
+    scoreText.setText('Score:' + score);
+
+    if (axo.countActive(true) === 0)
+    {
+        //  A new batch of stars to collect
+        axo.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+
+    }
+}
+
+function hitBomb (player, bomb)
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
 }
