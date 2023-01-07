7bd18785176be491eb8a7f55708eb9144d26e739
Lou Yonzon <Marytalens@gmail.com>
2023-01-06 17:36:55 -0800

    aa

--- src/main.js
+++ src/main.js
@@ -1,27 +1,11 @@
 let config = {
     type: Phaser.AUTO,
     width: 700,
-    height: 540
-    
- }
+    height: 540,
+    scene: [menu, play,death]
+}
 
-
- // set UI size
+ let game = new Phaser.Game(config);
+// set UI size
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('ocean', 'assets/Ocean.png')
-}
-
-function create ()
-{this.add.image(700,540,'ocean');
-
-}
-
-function update ()
-{
-}
