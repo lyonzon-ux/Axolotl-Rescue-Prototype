4857a6b746a445c962a3320414d195abfcafb9da
Lou Yonzon <Marytalens@gmail.com>
2023-01-06 17:15:32 -0800

    Edits on main

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
 let config = {
     type: Phaser.AUTO,
-    width: 480,
-    height: 640
+    width: 700,
+    height: 540
     
  }
 
- let game = new Phaser.Game(config);
-// set UI size
+
+ // set UI size
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('ocean', 'assets/Ocean.png')
+}
+
+function create ()
+{this.add.image(700,540,'ocean');
+
+}
+
+function update ()
+{
+}
