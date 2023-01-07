b738952cd506fad304489aeaa0554ba0b3009a80
Lou Yonzon <Marytalens@gmail.com>
2023-01-06 16:49:25 -0800

    did this thing

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ let config = {
     type: Phaser.AUTO,
     width: 480,
     height: 640
+    
  }
 
+ let game = new Phaser.Game(config);
 // set UI size
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
